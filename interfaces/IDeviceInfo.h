87305e576cab19144a915a3cc63d5062cc39e1ef
modeveci <o.deveci@metrological.com>
2021-03-02 14:50:49 +0100

    [deviceinfo]: update IDeviceInfo and merge with IDeviceIdentification

--- interfaces/IDeviceInfo.h
+++ interfaces/IDeviceInfo.h
@@ -1,3 +1,22 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2021 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 
 #include "Module.h"
@@ -6,11 +25,21 @@
 
 namespace WPEFramework {
 namespace Exchange {
+namespace IDeviceInfo {
+    
+    struct EXTERNAL IProperties : virtual public Core::IUnknown {
+        enum { ID = ID_DEVICE_PROPERTIES };
 
-    struct EXTERNAL IDeviceCapabilities : virtual public Core::IUnknown {
+        ~IProperties() override = default;
+
+        virtual const string Chipset() const = 0;
+        virtual const string FirmwareVersion() const = 0;
+    };
+
+    struct EXTERNAL ICapabilities : virtual public Core::IUnknown {
         enum { ID = ID_DEVICE_CAPABILITIES };
 
-        virtual ~IDeviceCapabilities() {}
+        ~ICapabilities() override = default;
 
         enum AudioOutput : uint8_t {
             AUDIO_OTHER,
@@ -72,5 +101,6 @@ namespace Exchange {
 
 
     };
+}
 }
 }
